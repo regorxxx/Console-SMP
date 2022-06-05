74b22b1189b2fa37e2fd740920b14c8eb316447e
regorxxx <regorxxx@protonmail.com>
2022-06-05 12:50:21 +0200

    Updated example.

--- examples/console.js
+++ examples/console.js
@@ -2,4 +2,7 @@
 include('..\\helpers\\helpers_xxx_console.js');
 
 console.log('This is output to log file at foobar profile folder and to console on UI.');
+// This simply replicates the original method
 console.logUI('This is output only to console on UI. New method.');
+// New lines '\n' are split into new entries before sending them to the console
+console.popup('This is output to log file at foobar profile folder, console on UI and a popup.\nNew method.', 'My popup');
